2c08c6870a6bcb82055dc04a0871bbd153953124
Clessio <clessio.silva1989@gmail.com>
2025-09-15 08:29:43 -0300

    Rabbit config

--- src/main/java/com/ctech/client_api/config/RabbitConfig.java
+++ src/main/java/com/ctech/client_api/config/RabbitConfig.java
@@ -1,5 +1,14 @@
 package com.ctech.client_api.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitConfig {
 
+	@Bean
+	Queue queue() {
+		return new Queue("clienteQueue", false);
+	}
 }
