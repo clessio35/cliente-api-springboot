0a5bdf472f673e0b8461f77dfcaa7e72101bd578
Clessio <clessio.silva1989@gmail.com>
2025-09-15 08:26:54 -0300

    client-controller

--- src/main/java/com/ctech/client_api/controller/ClienteController.java
+++ src/main/java/com/ctech/client_api/controller/ClienteController.java
@@ -1,12 +1,50 @@
 package com.ctech.client_api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ctech.client_api.model.Cliente;
+import com.ctech.client_api.service.ClienteService;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping
+	public List<Cliente> getAllClientes() {
+		return clienteService.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
+		Optional<Cliente> cliente = clienteService.findById(id);
+		return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
+		Cliente newClient = clienteService.save(cliente);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newClient);
+	}
 	
-	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteCliente(@PathVariable Long id){
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
