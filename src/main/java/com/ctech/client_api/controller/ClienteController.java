589777caba8dcb9b158447dc7ed201586f3d43fa
Clessio <clessio.silva1989@gmail.com>
2025-09-15 08:24:56 -0300

    client-service

--- src/main/java/com/ctech/client_api/controller/ClienteController.java
+++ src/main/java/com/ctech/client_api/controller/ClienteController.java
@@ -1,5 +1,12 @@
 package com.ctech.client_api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
+	
+	
 }
--- src/main/java/com/ctech/client_api/service/ClienteService.java
+++ src/main/java/com/ctech/client_api/service/ClienteService.java
@@ -1,5 +1,34 @@
 package com.ctech.client_api.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ctech.client_api.model.Cliente;
+import com.ctech.client_api.repository.ClienteRepository;
+
+@Service
 public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+
+	public Optional<Cliente> findById(Long id) {
+		return clienteRepository.findById(id);
+	}
+
+	public Cliente save(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+
+	public void delete(Long id) {
+		clienteRepository.deleteById(id);
+	}
 
 }
